83fc1477dff8898fbfd2d170bb63d7b89bd3bba5
liam <liam@nehoray.net>
2024-04-28 12:20:34 +0200

    Finished logic for displaying books

--- script.js
+++ script.js
@@ -18,7 +18,9 @@ let book2 = new Book('sams adventures', 'sam', 'earthdiaries')
 addBookToLibrary(book1)
 addBookToLibrary(book2)
 
-let books = myLibrary.map(function(book) {
+
+//Displaying array of books
+let books = myLibrary.map(function (book) {
     // Create a newbook with the 'book' class
     const newBook = document.createElement('div')
     newBook.classList.add('book')
@@ -26,6 +28,42 @@ let books = myLibrary.map(function(book) {
     // Adding title to the element
     let bookTitle = document.createElement('p')
     bookTitle.textContent = book.title
+
+    // Adding author to the element
+    let bookAuthor = document.createElement('p')
+    bookAuthor.textContent = book.author
+
+    // Adding publisher to the element
+    let bookPublisher = document.createElement('p')
+    bookPublisher.textContent = book.publisher
+    
+    // Read button
+    let readButtonContainer = document.createElement('div')
+    readButtonContainer.style.display = 'flex'
+    readButtonContainer.style.flexDirection = 'row'
+
+    let readButton = document.createElement('input')
+    readButton.setAttribute('type', 'checkbox')
+    let readButtonText  = document.createElement('p')
+    readButtonText.textContent = 'read?'
+
+    // Appending to readbutton
+    readButtonContainer.appendChild(readButton)
+    readButtonContainer.appendChild(readButtonText)
+
+    // remove button
+    let removeButton = document.createElement('button')
+    removeButton.textContent = 'Remove'
+
+    // Appending all elements
+    newBook.appendChild(bookTitle)
+    newBook.appendChild(bookAuthor)
+    newBook.appendChild(bookPublisher)
+    newBook.appendChild(readButtonContainer)
+    newBook.appendChild(removeButton)
+
+
+
 
     return newBook
 })
